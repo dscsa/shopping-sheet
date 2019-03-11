0ee48469885aabf2448e980cfd0a01cc5fd28d4b
akircher <adam@Adams-MacBook-Pro-2.local>
2019-03-11 15:24:08 -0400

    Debug #11121 showing erroneous Registration Pompt

--- Live Inventory.js
+++ Live Inventory.js
@@ -1,11 +1,11 @@
 function setV2info(drug) {
-  //Determines which price point to use, and does any necessary prorating    
+  //Determines which price point to use, and does any necessary prorating
   //Use 30 day price if available, otherwise use 90 day price.  Prorate using days supply
   if ( ! drug.$Gcn) {
     debugEmail('Gcn Error', drug) //spreadsheet ui cannot be triggered from trigger
     return //should al
   }
-  
+
   try {
     _setV2info(drug)
   } catch (e1) {
@@ -21,13 +21,13 @@ function setV2info(drug) {
 }
 
 function _setV2info(drug) {
-  
+
   var v2info = liveInventoryByGcn(drug)
-    
+
   //Log('v2info', v2info)
   if ( ! v2info['generic'])
     return drug.$Stock = +drug.$Gcn ? 'Not Offered' : 'No GCN'
-      
+
   drug.$v2        = v2info['generic']
   drug.$TotalQty  = +v2info['inventory.qty'] || 0 //drug.$AvailableQty = +v2info['Available Qty'] || 0
   drug.$RepackQty = +v2info['order.repackQty'] || 135
@@ -35,11 +35,11 @@ function _setV2info(drug) {
     drug.$Stock = 'No V2 Stock'
   else
     drug.$Stock = v2info.stock || undefined //empty string means drug is in-stock so put undefined so it doesn't show up in drug json
-    
+
   drug.$MonthlyPrice = +v2info['price / month']
-  
+
   var emailJson = JSON.stringify(v2info, null, " ")
-    
+
   if ( ! drug.$Stock && v2info['order.maxInventory'] & (v2info['inventory.qty'] < v2info['order.maxInventory']/2))
     sendEmail('Change Order for High stock Item', ['Consider updating v2 order for '+drug.$v2, drug.$v2+' ('+drug.$Name+') is high stock but its max inventory of '+v2info['order.maxInventory']+' is already way higher than its total quantity of '+v2info['inventory.qty']+' '+emailJson])
   else if ( ! drug.$Stock && v2info['inventory.qty'] > 1000 && (v2info['inventory.qty']/v2info['dispense.qty'] > 3) && (v2info['order.minQty'] < 5 || v2info['order.minDays'] < 150))
@@ -51,69 +51,71 @@ function _setV2info(drug) {
 }
 
 function isNotInOrder(drug, order) {
-  
+
   //Mirror the logic of when we are using useDispensed in SetDaysQtyRefills
   if (drug.$Type == 'Dispensed') return
-  
+
   if ( ! drug.$InOrder && drug.$NextRefill == 'Transferred Out')
     return 'was transferred out on '+drug.$RxChanged.slice(0, 10)+' and can be filled at your backup pharmacy'
-     
-  if ( ~ ['No V2 stock'].indexOf(drug.$Stock)) 
+
+  if ( ~ ['No V2 stock'].indexOf(drug.$Stock))
     return 'is not currently offered and was transferred to your local pharmacy'
-  
-  if ( ! +drug.$Gcn) 
+
+  if ( ! +drug.$Gcn)
     return 'needs to be checked to determine if it is available'
-    
+
   //Added because of Order #9554.  Meslamine was pended okay, but then a change of another drug, caused it to run again and this time the TotalQty was too low (because it had been pended) and gave patient a notification that it was too low to fill
   drug.$IsPended = !! openSpreadsheet('Shopping List #'+drug.$OrderId, 'Shopping Lists').getSheetByName(drug.$v2) //This should be cached so not too expensive
-       
+
   //Should we allow apparent one time fills (refills_left == 0) as well?
   if ( ! drug.$IsPended && ! drug.$IsRefill && ~ ['Out of Stock', 'Refills Only', 'Not Offered'].indexOf(drug.$Stock)) {
-    
+
        if ( ! drug.$Gcn) return 'appears to be out-of-stock but we are currently confirming'
-       
-       if (drug.$MonthlyPrice >= 20) return 'is unavailable for new RXs at this time' 
-       
+
+       if (drug.$MonthlyPrice >= 20) return 'is unavailable for new RXs at this time'
+
        return 'is unavailable for new RXs and was transferred to your local pharmacy' //Should we allow apparent one time fills (refills_left == 0) as well?
   }
-  
+
   //Testing this out
   if (drug.$NextRefill == 'Rx Expired') // Include this too???:  || ( ! drug.$InOrder && drug.$NextRefill == 'Rx Expiring')
     return 'has an expired Rx.  Please ask your doctor for a new Rx'
-     
-  if ( ! drug.$RefillsTotal) 
+
+  if ( ! drug.$RefillsTotal)
     return 'is out of refills.  Please contact your doctor'
 
   if ( ! drug.$InOrder && drug.$NextRefill == 'AutoRefill Off')
     return 'has automatic refills turned off.  Please request 2 weeks in advance'
-    
-  if (order.$Status == 'Needs Form')
+
+  if ( ! order.$Pharmacy) {//order.$Status == 'Needs Form' was messing up on #11121 since status showed as "Shopping" but this message still appeared
+    debugEmail('Patient needs to register email', '#'+drug.$OrderId, order.$Pharmacy, order.$Status, drug, order)
     return 'cannot be filled until patient registration is complete'
-    
+  }
+
   if ( ! drug.$Autofill.patient && drug.$AutoPopulated)
     return 'was requested but you have turned all medications off autorefill'
-    
+
   if ( ! drug.$InOrder && drug.$NextRefill == 'N/A')
     return 'is past due.  Please request 2 weeks in advance'
-    
-  if (new Date(drug.$NextRefill) - new Date(order.$OrderAdded) > maxMedSyncTime(drug)) 
+
+  if (new Date(drug.$NextRefill) - new Date(order.$OrderAdded) > maxMedSyncTime(drug))
     return 'is due for a refill on '+drug.$NextRefill
-    
-  if ( ! drug.$IsPended && drug.$TotalQty < 2000 && drug.$Qty > 2.5*drug.$RepackQty) { 
+
+  if ( ! drug.$IsPended && drug.$TotalQty < 2000 && drug.$Qty > 2.5*drug.$RepackQty) {
     return 'was prescribed in an unusually high qty and needs to be reviewed by a pharmacist'
   }
-  
-  if (drug.$TotalQty < drug.$Qty && ! drug.$IsPended) { 
-    
-     if (drug.$TotalQty >= 90) return 'is low in stock.  We will fill it as soon as we can.' //want it past tense on order invoice, but present tense and looser for refill reminder emails. 
-      
+
+  if (drug.$TotalQty < drug.$Qty && ! drug.$IsPended) {
+
+     if (drug.$TotalQty >= 90) return 'is low in stock.  We will fill it as soon as we can.' //want it past tense on order invoice, but present tense and looser for refill reminder emails.
+
      if (drug.$MonthlyPrice >= 20) return  'is out of stock.  We will fill it as soon as we can.' //Cindy not always transferring out branded medication right now so don't say that
-      
+
      return 'is out of stock right now and will be transferred out to your local pharmacy'
   }
-  
+
   //#4636 Webform Transfer Created Empty Order And Did Not Include Drugs From Profile
-  //#4618 An Auto Refill Appeared That Didn't Include A Drug That Had A Refill Date Of 2 Months Ago (why didn't we refill it then?) 
+  //#4618 An Auto Refill Appeared That Didn't Include A Drug That Had A Refill Date Of 2 Months Ago (why didn't we refill it then?)
   //#4607 Query Mistake?  IsRefill is true but no LastRefill
   //#4598 Scripts were written and sent on 12/17 but not filled right away.  So today on 5/25 we assume there was a reason we didn't send them
   //Put in Date comparison so that meds that should be medsynced are shopped for EVEN if they are not in the order
@@ -126,18 +128,18 @@ function isNotInOrder(drug, order) {
 
 var liveInventoryCache = {}
 function liveInventoryByGcn(drug) {
-  
+
   var gcn = drug.$Gcn
   //Create a map function for each GCN
   if ( ! Object.keys(liveInventoryCache).length) {
-   
+
     var sheet = getSheet('https://docs.google.com/spreadsheets/d/1gF7EUirJe4eTTJ59EQcAs1pWdmTm2dNHUAcrLjWQIpY/edit#gid=505223313', 'U', 1)
-    
+
     var genericNames  = sheet.colByKey('key.2')
-    
+
     if ( ! genericNames)
       throw new Error('Live Inventory Sheet Down.  Stopping Shopping Sheet!')
-      
+
     var inventoryQtys = sheet.colByKey('inventory.qty')
     var dispensedQtys = sheet.colByKey('dispensed.qty')
     var stockLevels   = sheet.colByKey('stock')
@@ -145,10 +147,10 @@ function liveInventoryByGcn(drug) {
     var minQtys       = sheet.colByKey('order.minQty')
     var minDays       = sheet.colByKey('order.minDays')
     var maxInventory  = sheet.colByKey('order.maxInventory')
-    
+
     for (var gcns in genericNames) {
       gcns = gcns.split(',')
-      for (var i in gcns) {  
+      for (var i in gcns) {
         liveInventoryCache[gcns[i]] = {
           'generic':genericNames[gcns],
           'inventory.qty':inventoryQtys[gcns],
@@ -160,11 +162,11 @@ function liveInventoryByGcn(drug) {
           'order.maxInventory':maxInventory[gcns]
         }
       }
-    } 
-    
+    }
+
     Log('liveInventoryByGcn 4', gcn, liveInventoryCache)
   }
-  
+
   if (drug.$InOrder && +gcn && ! liveInventoryCache[gcn]) {
     sendEmail('Could not find GCN in v2', [gcn, JSON.stringify(drug), JSON.stringify(liveInventoryCache)])
   }
@@ -182,10 +184,10 @@ var minDays
 var maxInventory
 
 function getV2info(v2drug) {
-  
+
   //Log('getV2info', v2drug)
   var sheet = getSheet('https://docs.google.com/spreadsheets/d/1gF7EUirJe4eTTJ59EQcAs1pWdmTm2dNHUAcrLjWQIpY/edit#gid=505223313', 'T', 1)
-  
+
   //Get and cache all the remote columns we will need
   v2drugs = v2drugs || sheet.colByKey('key.2')
   totalQtys = totalQtys || sheet.colByKey('inventory.qty')
@@ -194,7 +196,7 @@ function getV2info(v2drug) {
   minQtys = minQtys || sheet.colByKey('order.minQty')
   minDays = minDays || sheet.colByKey('order.minDays')
   maxInventory = maxInventory || sheet.colByKey('order.maxInventory')
-  
+
   return {
     'generic':v2drugs[v2drug],
     'inventory.qty':totalQtys[v2drug],
