ab1bf8fa02380683549cd6f1cf7f49dff29bcea5
akircher <adam@Adams-MacBook-Pro-2.local>
2019-03-11 19:31:19 -0400

    Add autofill changes to drug changes because of Orders like #11178

--- Import Report.js
+++ Import Report.js
@@ -3,10 +3,10 @@
 function importCSV(filename){
 
   var files = DriveApp.getFilesByName(filename)
-  
+
   if (files.hasNext()) {
     var file = files.next().getBlob().getDataAsString()
-    
+
     //https://stackoverflow.com/questions/632475/regex-to-pick-commas-outside-of-quotes
     return file.split(/\r\n/g).map(function(row) {
       //Replace escaped double quotes because we had one instance of JSON being put into a user_defined field which because of JSON's quotes and commas broke our CSV
@@ -23,14 +23,14 @@ function importCSV(filename){
 function importReport(filename, sheet) {
    var csv = importCSV(filename)
    //Log('importCSV', csv.length, csv)
-   if (csv.length < 6) //An empty report is 6 rows long 
+   if (csv.length < 6) //An empty report is 6 rows long
      throw Error('CSV file incomplete: '+JSON.stringify(csv, null, " "))
-     
+
    csv = csv.slice(3, -3).map(function(vals) {
      //Log('toObject', csv[1], vals)
-     return toObject(csv[1], vals) 
-   })  
-   
+     return toObject(csv[1], vals)
+   })
+
    return csv
 }
 
@@ -44,36 +44,36 @@ function getReport(filename, sheet) {
 //https://stackoverflow.com/questions/35810639/new-date-gives-invalid-date-in-app-script-but-works-fine-on-console
 //do not put in 'Z' or it will mess up timezones
 function toDate(date) {
-  
+
   if ( ! date || date.length != 23) //YYYY-MM-DD HH:MM:SS.MMM
     return date
-    
+
   return new Date(date.replace(' ', 'T').slice(0, -4))
 }
 
 function normalizeDrugs(report) {
-   
+
   //var sheet = getSheet('GCN', 'A', 1)
-  
+
   //var v2Names = sheet.colByKey('v2 Name')
-  
-  for (var i in report) { 
+
+  for (var i in report) {
     normalizeDrug(report[i])
     //addV2Name(report[i].drug, v2Names)
   }
 }
 
 function normalizeDrug(row) {
-  
+
   var dispenseDate = +row.is_refill ? row.dispense_date : row.orig_disp_date //See Order 10062.  Seems that orig_disp_date may get set before dispense date causing a mismatch.  Correct for that here.
-  
+
   var $IsDispensed = row.ship_date ? !!row.in_order : row.in_order && (new Date() - new Date(dispenseDate) < 4*24*60*60*1000)  //See Order #8590.  Risperidone 2mg was dispensed but it didn't register here and so because it was out of refills was excluded from the order //Order 10862 was shipped within 4 days of 10698, so showed Levothroxine and Metoprolol as dispensed even though they were in order 10698 and not 10862
-  var $InOrder = $IsDispensed || (row.in_order && +row.refills_total)   //Even if its "in the order" it could be a pending or denied surescript refill request (order 7236) so need to make sure refills are available  
+  var $InOrder = $IsDispensed || (row.in_order && +row.refills_total)   //Even if its "in the order" it could be a pending or denied surescript refill request (order 7236) so need to make sure refills are available
   var $InOrderNotDispensed = row.in_order && ! $IsDispensed
   var $RefillsLeft = +($InOrderNotDispensed ? +row.refills_left : +row.refills_total).toFixed(2) //if not in order or already shipped use total refills not just the last dispensed to avoid erroneous out of refills warning
-  
+
   row.drug = {
-    $Name:($InOrder ? '' : '* ')+row.drug_name.slice(1, -1).trim(), //remove quotes that protect commas, 
+    $Name:($InOrder ? '' : '* ')+row.drug_name.slice(1, -1).trim(), //remove quotes that protect commas,
     $Msg:undefined,    //placeholder for JSON ordering.
     $Days:null,        //placeholder for JSON ordering.
     $Qty:null,         //placeholder for JSON ordering.
@@ -82,9 +82,9 @@ function normalizeDrug(row) {
     $SyncBy:undefined, //placeholder for JSON ordering.
     $Stock:undefined,  //placeholder for JSON ordering.
     $RefillsLeft:$RefillsLeft, //if not in order or already shipped use total refills not just the last dispensed to avoid erroneous out of refills warning
-    $RefillsTotal:+(+row.refills_total).toFixed(2), 
+    $RefillsTotal:+(+row.refills_total).toFixed(2),
     $IsRefill:+row.is_refill,
-    $IsDispensed:$IsDispensed, 
+    $IsDispensed:$IsDispensed,
     $FirstRefill:row.orig_disp_date,
     $LastRefill:dispenseDate,
     $NextRefill:'N/A',
@@ -104,9 +104,9 @@ function normalizeDrug(row) {
     $RxExpires:toDate(row.expire_date),
     $Autofill:{rx:+row.rx_autofill, patient:+row.pat_autofill},
     $Scripts:{
-      ordered:row.ordered_script_no, 
-      high_refills:row.oldest_script_high_refills, 
-      with_refills:row.oldest_script_with_refills, 
+      ordered:row.ordered_script_no,
+      high_refills:row.oldest_script_high_refills,
+      with_refills:row.oldest_script_with_refills,
       newest:row.newest_script
     }
   }
@@ -114,14 +114,14 @@ function normalizeDrug(row) {
   if (row.script_status == 'Transferred Out') { //Or expired?
     row.drug.$Msg = 'Transferred Out'
     row.drug.$NextRefill = 'Transferred Out'
-  } 
+  }
   else if ( ! row.drug.$RefillsTotal) {
     row.drug.$NextRefill = 'No Refills'
     if ($InOrderNotDispensed) row.drug.$Msg = ' is in order but has no refills' //Could be a pending or denied surescript refill request (order 7236) so need to make sure refills are available
   }
   /*
   HOPEFULLY FIXED WITH THE var dispenseDate change above
-  else if ( ! row.drug.$RefillsLeft) { 
+  else if ( ! row.drug.$RefillsLeft) {
     row.drug.$LastRefill = new Date().toJSON().slice(0, 10) //Refills are deducted before $LastRefill date is updated.  Making it look like a drug with out any refills is in order.    //estimateNextRefill(drug)
     //This can happen if last refill was just dispensed AND if Cindy adds a 0 refills rx in order to request a sure script
     //sendEmail('adam.kircher@gmail.com', 'Last Refill Just Dispensed?', [JSON.stringify(row, null, '  ')])
@@ -135,7 +135,7 @@ function normalizeDrug(row) {
     row.drug.$NextRefill = row.autofill_date.slice(0, 10)
     row.drug.$DaysUntilRefill = new Date(row.drug.$NextRefill) - toDate(row.order_added)
     if (row.drug.$DaysUntilRefill > minMedSyncTime(row.drug) && row.drug.$DaysUntilRefill < maxMedSyncTime(row.drug)) {
-      
+
       var verb = ' will be'
       if ( ! row.drug.$InOrder) {
         verb = ' may be'
@@ -147,14 +147,14 @@ function normalizeDrug(row) {
   }
   else if (row.user_def_1.slice(1, -1) && ( ! +row.rx_autofill || ! +row.pat_autofill)) { //Has registered (backup pharmacy) but autofill was turned off (Note: autofill is off until a patient registers)
     row.drug.$NextRefill = 'AutoRefill Off'
-    
+
     if ($InOrder) row.drug.$Msg = row.drug.$AutoPopulated ? 'has autorefill off but was just requested to be filled' : 'has autorefill off but was requested by you'  //Someone called in to request a med off autofill or a doctor sent one in (not sure if we should send the latter but better safe than sorry???).  Keeping second option ambiguous right now: to add "requessted by your doctor" we would need to check date_written.  Checking FirstRefill is giving us some false postives (e.g "requested by your doctor" but should be "by you")
-  } 
+  }
   else if (row.refill_date.slice(0,10) >= toDate(row.order_added).toJSON().slice(0, 10)) { //else if (row.drug.$LastRefill) estimateNextRefill(drug)
     row.drug.$NextRefill = row.refill_date.slice(0,10)
     row.drug.$DaysUntilRefill = new Date(row.drug.$NextRefill) - toDate(row.order_added)
     if (row.drug.$DaysUntilRefill > minMedSyncTime(row.drug) && row.drug.$DaysUntilRefill < maxMedSyncTime(row.drug)) {
-      
+
       var verb = ' will be'
       if ( ! row.drug.$InOrder) {
         verb = ' may be'
@@ -167,7 +167,11 @@ function normalizeDrug(row) {
   else if (row.drug.$RxExpires - toDate(row.order_added) <= Math.max(45, row.drug.$DaysSupply)*24*60*60*1000) { //This needs to be after AutoFill Off because of #10662
     row.drug.$Refills = row.drug.$NextRefill = 'Rx Expiring'
     row.drug.$Msg = 'will expire soon, ask your doctor for a new Rx'
-  } 
+  }
+
+  if (row.invoice_nbr == '11121') {
+    debugEmail('WHAT IS GOING ON', '#'+row.invoice_nbr, row.drug.$Autofill, {rx:row.rx_autofill, patient:row.pat_autofill}, row)
+  }
 }
 
 /*
@@ -180,39 +184,39 @@ function estimateNextRefill(drug) {
 
 /*
 function addV2Name(drug, v2Names) {
-  
+
   if ( ! v2Names[drug.$Gcn])
     drug.$Stock = 'Update GCN'
-  
+
   drug.$v2 = v2Names[drug.$Gcn] || '' //use empty string so string.replace doesn't cause issues later on in the code
 }*/
 
 function groupByOrder(report) {
   //Log('report', report.length, report.reverse())
-   
+
   var group = {}
   //Log('Log A', report)
   //since we are prepending go backwards to maintain order
-  for (var i in report.reverse()) { 
-    
+  for (var i in report.reverse()) {
+
     if (Object.keys(report[i]).length < 4) continue
-    
-    if ( ! report[i].invoice_nbr) continue //Now that we include Transfer Outs.  Some patients may not have any Order Id (only one drug and it was never filled), skip these for now, because this currently causes shopping sheet to go crazy and keep adding blank rows 
-    
+
+    if ( ! report[i].invoice_nbr) continue //Now that we include Transfer Outs.  Some patients may not have any Order Id (only one drug and it was never filled), skip these for now, because this currently causes shopping sheet to go crazy and keep adding blank rows
+
     if (report[i].script_status == 'Inactive') continue //skip inactive Rxs for right now
-    
+
     var orderID = report[i].invoice_nbr
-   
+
     //Log('groupByOrder', report[i].invoice_nbr, group[orderID])
-    
+
     if ( ! group[orderID]) {
       group[orderID] = newGroup(report[i])
       //Log('groupByOrder', report[i].invoice_nbr, group[orderID])
     }
-    
+
     //Logger.log(['Log B', group[orderID]])
     if (report[i].drug.$ScriptNo) //some lines are empty orders with no drugs
-      group[orderID].$Drugs.push(report[i].drug)  
+      group[orderID].$Drugs.push(report[i].drug)
     //addDrugtoOrder(group[orderID], report[i].drug)
   }
 
@@ -222,22 +226,22 @@ function groupByOrder(report) {
 //If not in order, don't show if there is a similar drug (even different strengths) already in the order
 //For this to work drugs need to be sorted with not included drugs being last, which is not in the report
 /*function addDrugtoOrder(group, drug) {
-   
+
   if ( ! drug.$Gcn) {
     drug.$Stock = 'No GCN'
     if (drug.$ScriptNo) debugEmail('No Gcn Error', drug)
     return //Missing Rx Orders
   }
-  
+
   if ( ! drug.$InOrder) {
     var needle = drug.$v2.replace(/ [\d.]+/, '')
-    for (var i in group.$Drugs) {        
-      if (needle == group.$Drugs[i].$v2.replace(/ [\d.]+/, '')) return 
+    for (var i in group.$Drugs) {
+      if (needle == group.$Drugs[i].$v2.replace(/ [\d.]+/, '')) return
     }
   }
-  
+
   //if (group.$OrderId == 4367) debugEmail('addDrugtoOrder', drug.$v2, needle, group.$Drugs)
-  group.$Drugs.push(drug)  
+  group.$Drugs.push(drug)
 }*/
 
 function newGroup(row) {
@@ -245,19 +249,19 @@ function newGroup(row) {
   var paymentInfo  = row.user_def_4.slice(1, -1).split(',')
   var rxSource     = row.order_category == 3 ? 'Transfer' : 'eRX'
   var pharmacyName = row.user_def_1.slice(1, -1).replace(/ #|-\d|\d-|\d/g, '').replace(/\s{2,}/g, ' ')  //Remove digits, pound sign, and hyphens (Store Number) from pharmacy name
-  
+
   var now = new Date()
   //if (now.getHours() == 17 && now.getMinutes() < 3 && (row.tracking_code || row.ship_date))
   //  sendEmail('adam.kircher@gmail.com', 'Order to be shipped', ['order #', row.invoice_nbr, 'tracking_code', row.tracking_code, 'ship_date', row.ship_date, 'row', JSON.stringify(row, null, '  ')])
 
-  return {      
+  return {
     $OrderId:row.invoice_nbr,
     $Drugs:[],
     $New:+row.is_current_patient ? undefined : rxSource, //2 is eRx, 3 is pharmacy, NULL is SureScript or Cindy Manually Entered //Delete later if not new
     $Coupon:paymentInfo[3],
     $Card:paymentInfo[2] && paymentInfo[0] ? paymentInfo[2]+' '+paymentInfo[0] : '',
     $Lang:row.primary_language_cd,
-    $Pharmacy:pharmacyName ? pharmacyName+' on '+pharmacyInfo[3]+' ('+pharmacyInfo[2]+')' : '', 
+    $Pharmacy:pharmacyName ? pharmacyName+' on '+pharmacyInfo[3]+' ('+pharmacyInfo[2]+')' : '',
     $PatientAdded:toDate(row.patient_added),
     $OrderDispensed:row.ship_date ? toDate(row.ship_date) : '',
     $OrderAdded:toDate(row.order_added),
--- didDrugsChange.js
+++ didDrugsChange.js
@@ -1,55 +1,61 @@
 //BECAUSE THIS IS RUN BEFORE ADDDRUGDETAILS (e.g Before Incorporating Live Inventory such as TotalQty, Low Stock, Refills Only, Out of Stock, etc) IT WILL
 //NOT DETECT DRUGS THAT ARE "In Order" but go from 0 Days to >0 Days or go from >0 Days to 0 Days because of Live Inventory
 function didDrugsChange(newDrugs, oldDrugs, $Status) {
-    
+
   var changes = []
-  
+
   //debugEmail('BEFORE didDrugsChange', changes, '$Status', $Status, 'newDrugs', newDrugs, 'oldDrugs', oldDrugs)
 
-  //https://stackoverflow.com/questions/21987909/difference-between-two-array-of-objects-in-javascript  
+  //https://stackoverflow.com/questions/21987909/difference-between-two-array-of-objects-in-javascript
   for (var i in newDrugs) {
-    
+
     var newDrug   = newDrugs[i]
     var drugAdded = newDrug.$Name+' ADDED TO PROFILE'+(newDrug.$InOrder ? ' AND ORDER' : '')
-        
+
     for (var j in oldDrugs) {
-      
+
       var oldDrug = oldDrugs[j]
-      
+
       //Used to use GCN comparison but in rare cases Cindy would add two scripts of the same GCN into an order and then
       //didDrugChange would get triggered on every run because the refills would keep switching back and forth
       if (newDrug.$ScriptNo != oldDrug.$ScriptNo) continue
-      
+
       drugAdded = false //Match found so this is NOT a new drug
-        
+
       if (oldDrug.$InOrder && ! newDrug.$InOrder) //Can't do this "|| (oldDrug.$Days > 0 && newDrug.$Days === 0)" because setDaysQtyRefills has not run yet so newDrug.$Days might be null because it hasn't been set yet
         changes.push(newDrug.$Name+' REMOVED FROM ORDER')
-        
+
       if (newDrug.$InOrder && ! oldDrug.$InOrder) //Can't do this "|| (newDrug.$Days > 0 && oldDrug.$Days === 0)" because setDaysQtyRefills has not run yet so newDrug.$Days might not be 0 yet
         changes.push(newDrug.$Name+' ADDED TO ORDER')
-        
+
       else if ((newDrug.$RefillsLeft != oldDrug.$RefillsLeft) || (newDrug.$RefillsTotal != oldDrug.$RefillsTotal)) //"else if" because refills_left often change when adding a drug to an order - no need to double count
         changes.push(newDrug.$Name+' REFILLS CHANGED RefillsLeft:'+oldDrug.$RefillsLeft+' -> '+newDrug.$RefillsLeft+' RefillsTotal: '+oldDrug.$RefillsTotal+' -> '+newDrug.$RefillsTotal)
-        
+
+      if (newDrug.$Autofill.rx == 0 && oldDrug.$Autofill.rx == 1) //Can't do this "|| (newDrug.$Days > 0 && oldDrug.$Days === 0)" because setDaysQtyRefills has not run yet so newDrug.$Days might not be 0 yet
+        changes.push(newDrug.$Name+' AUTOFILL TURNED OFF')
+
+      if (newDrug.$Autofill.rx == 1 && oldDrug.$Autofill.rx == 0) //Can't do this "|| (newDrug.$Days > 0 && oldDrug.$Days === 0)" because setDaysQtyRefills has not run yet so newDrug.$Days might not be 0 yet
+        changes.push(newDrug.$Name+' AUTOFILL TURNED ON')
+
       if (newDrug.$Days != null && newDrug.$Days != oldDrug.$Days)
         changes.push(newDrug.$Name+' DAYS CHANGED '+oldDrug.$Days+' -> '+newDrug.$Days)
-        
+
       if (newDrug.$Qty != null && newDrug.$Qty != oldDrug.$Qty)
         changes.push(newDrug.$Name+' QTY CHANGED '+oldDrug.$Qty+' -> '+newDrug.$Qty)
-          
+
       if (newDrug.$Price != null && newDrug.$Price != oldDrug.$Price)
         changes.push(newDrug.$Name+' PRICE CHANGED '+oldDrug.$Price+' -> '+newDrug.$Price)
-        
-      if (newDrug.$Stock != oldDrug.$Stock && ~ ['No GCN', 'No V2 Stock', 'Shopping Sheet Error', 'Sig Parse Error'].indexOf(oldDrug.$Stock)) //undefined != undefined, most statuses not yet set but this attempt to catch No GCNs and Shopping Sheet Error messages that appear or disappear.  Also can include "'Not Offered" status because that gets set latter so we will keep retriggering "'Not Offered" -> undefined 
+
+      if (newDrug.$Stock != oldDrug.$Stock && ~ ['No GCN', 'No V2 Stock', 'Shopping Sheet Error', 'Sig Parse Error'].indexOf(oldDrug.$Stock)) //undefined != undefined, most statuses not yet set but this attempt to catch No GCNs and Shopping Sheet Error messages that appear or disappear.  Also can include "'Not Offered" status because that gets set latter so we will keep retriggering "'Not Offered" -> undefined
         changes.push(newDrug.$Name+' MSG CHANGED '+oldDrug.$Msg+' -> '+newDrug.$Msg+' STOCK CHANGED '+oldDrug.$Stock+' -> '+newDrug.$Stock)
-        
+
       Log(changes.length ? 'Drug Changed!' : 'No Drug Changes', newDrug, oldDrug, changes)
     }
-   
+
     if (drugAdded) //Because outer loop is only for newDrugs we only get drug ADDED to profile and not drugs REMOVED from profile
       changes.push(drugAdded)
   }
-  
+
   if (changes.length) {
      debugEmail('DrugsChanged', changes, '$Status', $Status, 'newDrugs', newDrugs, 'oldDrugs', oldDrugs)
      return changes
@@ -63,4 +69,3 @@ function sortDrugs(a, b) {
   if (b.$Name < a.$Name) return 1
   return 0
 }
-     
