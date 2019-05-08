fa823a6c271c0eb906f41ee5497ae973ca2b86ff
akircher <adam@Adams-MacBook-Pro-2.local>
2019-05-08 11:49:37 -0400

    Improve alert messages

--- Live Inventory.js
+++ Live Inventory.js
@@ -41,9 +41,9 @@ function _setV2info(drug) {
   var emailJson = JSON.stringify(v2info, null, " ")
 
   if ( ! drug.$Stock && v2info['order.maxInventory'] && (v2info['order.maxInventory'] > 3500) && (v2info["price / month"] < 20) && (v2info['order.maxInventory']/v2info['qty threshold'] > 5))
-    sendEmail('Change Order for High stock Item', ['Consider updating v2 order for '+drug.$v2, drug.$v2+' ('+drug.$Name+') is high stock but its max inventory of '+v2info['order.maxInventory']+' is already way higher than its total quantity of '+v2info['inventory.qty']+' '+emailJson])
+    sendEmail('Change Order for High stock Item', ['Consider updating v2 order for '+drug.$v2, drug.$v2+' ('+drug.$Name+') is high stock but its max inventory of '+v2info['order.maxInventory']+' is already way higher than its qty threshold of '+v2info['qty threshold']+' '+emailJson])
   else if ( ! drug.$Stock && (v2info['inventory.qty'] > 1000) && (v2info["price / month"] < 20) && (v2info['inventory.qty']/v2info['qty threshold'] > 5) && (v2info['order.minQty'] < 5 || v2info['order.minDays'] < 150))
-    sendEmail('Change Order for High stock Item', ['Consider updating v2 order for '+drug.$v2, drug.$v2+' ('+drug.$Name+') is high stock but is ordered even if it has a quantity > '+v2info['order.minQty']+' and expires more than '+v2info['order.minDays']+' days from now'+' '+emailJson])
+    sendEmail('Change Order for High stock Item', ['Consider updating v2 order for '+drug.$v2, drug.$v2+' ('+drug.$Name+') is high stock with a inventory of '+v2info['inventory.qty']+'but is ordered even if it has a quantity > '+v2info['order.minQty']+' and expires more than '+v2info['order.minDays']+' days from now'+' '+emailJson])
   else if (drug.$Stock && drug.$Stock != 'Not Offered' && (v2info['inventory.qty'] > v2info['order.maxInventory'] - 100))
     sendEmail('Change Order for Low stock Item', ['Consider updating v2 order for '+drug.$v2, drug.$v2+' ('+drug.$Name+') is '+drug.$Stock+' but its max inventory of '+v2info['order.maxInventory']+' is too low given it already has a total quantity of '+v2info['inventory.qty']+' '+emailJson])
   else if (drug.$Stock && drug.$Stock != 'Not Offered' && (v2info['order.minQty'] > 1 || v2info['order.minDays'] > 90))
@@ -149,6 +149,7 @@ function liveInventoryByGcn(drug) {
 
     var inventoryQtys = sheet.colByKey('inventory.qty')
     var dispensedQtys = sheet.colByKey('dispensed.qty')
+    var enteredQtys   = sheet.colByKey('entered.qty')
     var qtyThreshold  = sheet.colByKey('qty threshold')
     var stockLevels   = sheet.colByKey('stock')
     var monthlyPrices = sheet.colByKey('price / month')
@@ -162,6 +163,7 @@ function liveInventoryByGcn(drug) {
         liveInventoryCache[gcns[i]] = {
           'generic':genericNames[gcns],
           'inventory.qty':inventoryQtys[gcns],
+          'entered.qty':enteredQtys[gcns],
           'dispensed.qty':dispensedQtys[gcns],
           'qty threshold':qtyThreshold[gcns],
           'stock':stockLevels[gcns],
