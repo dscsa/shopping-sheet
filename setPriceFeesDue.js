4beabbd492d3c5e21e00ebb80c8753f6907074a9
akircher <adam@Adams-MacBook-Pro-2.local>
2019-05-11 16:52:51 -0400

    Correct $Price error in a different way

--- setPriceFeesDue.js
+++ setPriceFeesDue.js
@@ -18,9 +18,9 @@ function setFee(order) {
 }
 
 function setTotal(order) {
-  return order.$Total = order.$Drugs.reduce(function(sum, drug) { return sum + (setPrice(drug) || 0) }, 0)
+  return order.$Total = order.$Drugs.reduce(function(sum, drug) { return sum + setPrice(drug) }, 0)
 }
 
 function setPrice(drug) {
-  return drug.$Price = +Math.max(drug.$Days * drug.$MonthlyPrice / 30, drug.$Days ? 1 : 0).toFixed(0) //Minimum price of $1 (CK suggestion).  2019-01-28 Changed $Excluded to $Days because of Order 8235 and 8291
+  return drug.$Price = +Math.max(drug.$Days * drug.$MonthlyPrice / 30, drug.$Days ? 1 : 0).toFixed(0) || 0 //Minimum price of $1 (CK suggestion).  2019-01-28 Changed $Excluded to $Days because of Order 8235 and 8291
 }
