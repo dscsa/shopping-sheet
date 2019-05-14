6a9b06980d69656170804f2df3bc34455275bd3c
akircher <adam@Adams-MacBook-Pro-2.local>
2019-05-14 12:55:31 -0400

    Improve error message

--- Live Inventory.js
+++ Live Inventory.js
@@ -41,7 +41,7 @@ function _setV2info(drug) {
   var emailBody = []
 
   if ( ! drug.$Stock && v2info['order.maxInventory'] && (v2info['order.maxInventory'] > 3500) && (v2info["price / month"] < 20) && (+v2info['order.maxInventory'] > +v2info['dispensed.qty']+5*v2info['qty threshold']))
-    emailBody.push(['Consider decreasing v2 max inventory for '+drug.$v2, drug.$v2+' ('+drug.$Name+') '+v2info['order.maxInventory']+' > '+v2info['dispensed.qty']+' + 5*'+v2info['qty threshold']])
+    emailBody.push(['Consider decreasing v2 max inventory for '+drug.$v2, drug.$v2+' ('+drug.$Name+') to '+Math.ceil((+v2info['dispensed.qty']+5*v2info['qty threshold'])/1000)*1000+' '+v2info['order.maxInventory']+' > '+v2info['dispensed.qty']+' + 5*'+v2info['qty threshold']])
   else if ( ! drug.$Stock && (v2info['inventory.qty'] > 1000) && (v2info["price / month"] < 20) && (+v2info['inventory.qty'] > +v2info['dispensed.qty']+5*v2info['qty threshold']) && (v2info['order.minQty'] < 5 || v2info['order.minDays'] < 150))
     emailBody.push(['Consider increasing v2 minDays & minQty for '+drug.$v2, drug.$v2+' ('+drug.$Name+') '+v2info['inventory.qty']+' > '+v2info['dispensed.qty']+' + 5*'+v2info['qty threshold']])
   else if (drug.$Stock && drug.$Stock != 'Not Offered' && (v2info['inventory.qty'] > v2info['order.maxInventory'] - 100))
