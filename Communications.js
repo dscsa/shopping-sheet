692d9f1e0d6b638819e0d482eeffdcd57a733ee4
akircher <adam@Adams-MacBook-Pro-2.local>
2019-06-21 13:06:27 -0400

    Debug Logging

--- Communications.js
+++ Communications.js
@@ -425,6 +425,8 @@ function newPatientFollowup(order, groups) {
 
   if ( ! order.$New) return
 
+  if ( ! order.$Total) debugEmail('newPatientFollowup has not $Total', order)
+
   var numFills   = groups.FILL_ACTION.length + groups.FILL_NOACTION.length
   var numNoFills = groups.NOFILL_ACTION.length + groups.NOFILL_NOACTION.length
 
